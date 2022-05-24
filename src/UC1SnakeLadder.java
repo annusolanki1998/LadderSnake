39dad6b8c2d9039e87f5e4ba78820b411c7313b0
Annu <annusinghraj1998@gmail.com>
2022-05-24 17:38:53 +0530

    UC1 Snake Ladder Proble Commit

--- src/UC1SnakeLadder.java
+++ src/UC1SnakeLadder.java
@@ -1,6 +1,7 @@
-public class SnakeLadderUC1 {
+public class UC1SnakeLadder {
     public static void main(String[] args) {
         int start = 0;
         System.out.println("Single Player at start position 0 " );
     }
 }
+
